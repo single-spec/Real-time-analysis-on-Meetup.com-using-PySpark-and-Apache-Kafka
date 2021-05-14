b15fce449d966a50f75031f6289e02fe367f361e
Sonal Ingle <68352812+single-spec@users.noreply.github.com>
2021-05-14 19:23:22 -0400

    Add files via upload

---
+++ kafka_consumer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#Created by Sonal Ingle
+
+from kafka import KafkaConsumer
+import json
+
+# Subscribing a consumer to listen to topic 'meetup'
+meetup_consumer = KafkaConsumer('meetup', group_id = '1', bootstrap_servers = ['localhost:9092'])
+
+# Printing it for verification
+for message in meetup_consumer:
+	print (type(message.value))
