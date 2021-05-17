3bf95a7a659fc74d38f183cfa12e7aa8463ff951
Sonal Ingle <68352812+single-spec@users.noreply.github.com>
2021-05-17 18:05:45 -0400

    Update kafka_consumer.py

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -4,9 +4,9 @@
 from kafka import KafkaConsumer
 import json
 
-# Subscribing a consumer to listen to topic 'meetup'
-meetup_consumer = KafkaConsumer('meetup', group_id = '1', bootstrap_servers = ['localhost:9092'])
+# Subscribing a consumer to listen to topic1
+meetup_consumer = KafkaConsumer('topic1', group_id = '1', bootstrap_servers = ['localhost:9092'])
 
-# Printing it for verification
+# Printing the message
 for message in meetup_consumer:
 	print (type(message.value))
